4b6ce401228a1dd8d5d347f4d9988dc38d905989
Sahil Gunake <sahilgunake@gmail.com>
2022-05-19 19:36:43 +0530

    CORS update

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,17 @@ namespace ManagementService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "AllowOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+            });
+
             services.AddTransient<IStaffRepository, StaffRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -47,6 +58,8 @@ namespace ManagementService
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("AllowOrigin");
 
             app.UseAuthorization();
 
